f123fdc5bb42e799a1582b416bb31c2b112eeadb
Stepan <56102302+sereberiezhka@users.noreply.github.com>
2025-02-17 00:26:34 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
 
+TOKEN = "8092613164:AAFl8KoICDMZK0YDo4XWKIVVlqNSqh4HEV4"  # Твой токен от BotFather
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
